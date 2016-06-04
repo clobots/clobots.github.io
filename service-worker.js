c1699f8ea76e2d67507c351180bf908fa38190d5
Sivaprakash Ragavan <mail@sivragav.com>
2016-06-03 23:51:52 -0400

    Updating to the latest

--- service-worker.js
+++ service-worker.js
@@ -29,7 +29,7 @@
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["/index.html","7732d259fb3801f4a68006bdc020e847"]];
+var PrecacheConfig = [["/index.html","96c00639af0b4665d2b1ad0f296125ea"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1--' + (self.registration ? self.registration.scope : '') + '-';
 
